772ab2582966006f7c58a6ba29159c943bdfa2fc
tapash22 <tapasp263@gmail.com>
2025-01-22 21:30:51 +0600

    update with adding about and car detalis page

---
+++ src/components/carousel/CarCarousel.vue
@@ -0,0 +1,36 @@
+<template>
+    <carousel :perPageCustom="[
+        [480, 1],
+        [768,2],
+        [992,3],
+        [1200,3]
+    ]" :navigate-to="someLocalProperty" :mouse-drag="false" paginationActiveColor="red" paginationSize="10" paginationPadding="5" paginationColor="blue">
+        <slide style="padding:10px;" v-for="i in carGallery" :key="i.id" >
+            <div class="w-full h-auto p-2 flex justify-center items-center " >
+               <img :src="i.image" class="bg-cover bg-center rounded-xl h-[300px] w-full"/>
+            </div>
+        </slide>
+
+    </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+
+    props:['carGallery'],
+    components: {
+        Carousel,
+        Slide
+    }
+};
+
+</script>
+
+<style >
+.VueCarousel-pagination{
+    align-items:left;
+    margin-left:-47%;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,81 +1,91 @@
 <template>
   <div class="about">
-    <div class="container my-5">
-      <div class="row " style="height:200px;">
-        <div class="col align-self-start">
-          hi
+    <div
+      class="container flex justify-between items-center w-full h-auto py-5"
+      v-for="about in aboutInfo"
+      :key="about.id"
+    >
+      <div
+        class="flex justify-between items-center w-full"
+        v-if="!about.messages"
+      >
+        <div class="w-1/2">
+          <img :src="about.image" class="bg-cover bg-center" />
         </div>
-        <div class="col-6 align-self-end">
-          hi
-        </div>
-        <div class="col align-self-center">
-          hi
-        </div>
-        <div class="col align-self-start">
-          hi
-        </div>
-      </div>
-
-      <div class="clearfix">
-        <img src="@/assets/car.jpg" class="col-md-3 float-md-end mb-3 ms-md-4" alt="...">
-
-        <p>
-          A paragraph of placeholder text. We're using it here to show the use of the clearfix class. We're adding quite a
-          few meaningless phrases here to demonstrate how the columns interact here with the floated image.
-        </p>
-
-        <p>
-          As you can see the paragraphs gracefully wrap around the floated image. Now imagine how this would look with
-          some actual content in here, rather than just this boring placeholder text that goes on and on, but actually
-          conveys no tangible information at. It simply takes up space and should not really be read.
-        </p>
-
-        <p>
-          And yet, here you are, still persevering in reading this placeholder text, hoping for some more insights, or
-          some hidden easter egg of content. A joke, perhaps. Unfortunately, there's none of that here.
-        </p>
-      </div>
-
-      <div class="row gx-5 ">
-        <div class="col">
-          <div class="p-3 border bg-light text-center">Custom column padding</div>
-        </div>
-        <div class="col">
-          <div class="p-3 border bg-light">Custom column padding</div>
-        </div>
-      </div>
-
-      <div class="row d-flex justify-content-center">
-        <div  class="bg-success p-2 text-danger bg-opacity-100">
-          <p class="text-start shadow  bg-primary">
-            hi
+        <div class="block w-1/2 h-auto p-2">
+          <h2 class="text-xl font-bold text-black text-start mb-3 uppercase">
+            {{ about.title }}
+          </h2>
+          <p class="textt-sm font-normal text-gray-500 text-justify">
+            {{ about.subtitle }}
           </p>
-          <p class="text-end shadow-sm bg-secondary">
-            hi
+          <p class="text-sm font-normal text-black text-justify py-3">
+            {{ about.description }}
           </p>
-          <p class="text-center shadow-lg bg-info ">
-            hi3
-          </p>
+          <span class="text-sm font-thin text-gray-700">
+            {{ about.tag }}
+          </span>
         </div>
       </div>
 
-      <div class="bg-info clearfix">
-  <button type="button" class="btn btn-secondary float-start">Example Button floated left</button>
-  <button type="button" class="btn btn-secondary float-end">Example Button floated right</button>
-</div>      
-
+      <div class="flex justify-between items-center w-full" v-else>
+        <div class="block w-1/2 h-auto p-4">
+          <h2 class="text-xl font-bold text-black text-start mb-3 uppercase">
+            {{ about.title }}
+          </h2>
+          <p class="textt-sm font-normal text-gray-500 text-justify">
+            {{ about.subtitle }}
+          </p>
+          <hr class="my-3" />
+          <ul class="block w-full h-auto">
+            <li
+              class="flex justify-start items-center gap-2"
+              v-for="message in about.messages"
+              :key="message"
+            >
+              <i class="bi bi-check text-blue-500 text-lg font-semibold"></i>
+              <span class="text-sm font-medium text-black">
+                {{ message }}
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div class="w-1/2">
+          <img :src="about.image" class="bg-cover bg-center" />
+        </div>
+      </div>
     </div>
 
+    <div class="w-full h-auto bg-gray-900 p-5">
+      <CarCarousel :carGallery="carGallery" />
+    </div>
   </div>
 </template>
 
 <script>
+import { aboutInfo, carGallery } from "@/store/data";
+import CarCarousel from "@/components/carousel/CarCarousel.vue";
 export default {
+  data() {
+    return {
+      aboutInfo,
+      carGallery,
+    };
+  },
 
-}
+  components: {
+    CarCarousel,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.about {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  display: block;
+}
 /* .col,
 .col-6 {
   background: red;
